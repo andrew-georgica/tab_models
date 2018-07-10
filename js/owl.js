6afe699fb606fe115485508ebcb979ca2566aab0
Andrii Georgica <georgica.andrii@gmail.com>
2018-07-10 18:57:29 +0300

    Add Menu and Header sections

---
+++ js/owl.js
@@ -0,0 +1,74 @@
+
+$(document).ready(function() {
+
+  $("#owl-1").owlCarousel({
+
+      autoPlay: 35000, //Set AutoPlay to 3 seconds
+
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,2],
+      itemsTablet : [768, 2],
+      itemsMobile : [479, 1]
+  });
+
+});
+
+$(document).ready(function() {
+
+  $("#owl-2").owlCarousel({
+
+      autoPlay: 15000, //Set AutoPlay to 3 seconds
+
+      items : 1,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,1],
+      itemsTablet : [768, 1]
+  });
+
+});
+
+$(document).ready(function() {
+
+  $("#owl-3").owlCarousel({
+
+      autoPlay: 15000, //Set AutoPlay to 3 seconds
+
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,2],
+      itemsTablet : [768, 2],
+      itemsMobile : [479, 1]
+  });
+
+});
+
+$(document).ready(function() {
+
+  $("#owl-4").owlCarousel({
+
+      autoPlay: 15000, //Set AutoPlay to 3 seconds
+
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,2],
+      itemsTablet : [768, 2],
+      itemsMobile : [479, 1]
+  });
+
+});
+
+$(document).ready(function() {
+
+  $("#owl-5").owlCarousel({
+
+      autoPlay: 15000, //Set AutoPlay to 3 seconds
+
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,2],
+      itemsTablet : [768, 2],
+      itemsMobile : [479, 1]
+  });
+
+});
